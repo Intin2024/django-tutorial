3d2713c4c4c21588e740d66e751aeafe5e645a50
Antônio Ricart <ricarteusuo11@gmail.com>
2024-11-18 09:23:37 -0300

    Adiciona exemplo simples de novo registro

--- musicas/views.py
+++ musicas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Gravadora
 
 def home(request):
@@ -9,4 +9,12 @@ def list_records(request):
     return render(request, 'list_gravadoras.html', {"gravadoras": gravadoras})
 
 def create_records(request):
+    if request.method=="POST":
+        nome = request.POST.get('nome')
+        endereco = request.POST.get('endereco')
+        gravadora = Gravadora()
+        gravadora.nome = nome
+        gravadora.endereco = endereco
+        gravadora.save()
+        return redirect('lista-gravadoras')
     return render(request, 'form_gravadoras.html')
