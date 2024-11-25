23f27c386cfbdbf84a6889a330fccafce8f42451
Antônio Ricart <ricarteusuo11@gmail.com>
2024-11-25 07:49:33 -0300

    Adiciona exemplo de edição de recursos

--- musicas/urls.py
+++ musicas/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('gravadoras', views.list_records, name='lista-gravadoras'),
-    path('gravadoras/novo', views.create_records, name='cria-gravadoras')
+    path('gravadoras/novo', views.create_records, name='cria-gravadoras'),
+    path('gravadoras/editar/<int:id>', views.update_records, name='edita-gravadoras')
 ]
--- musicas/views.py
+++ musicas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Gravadora
 
 def home(request):
@@ -18,3 +18,15 @@ def create_records(request):
         gravadora.save()
         return redirect('lista-gravadoras')
     return render(request, 'form_gravadoras.html')
+
+
+def update_records(request, id):
+    gravadora = get_object_or_404(Gravadora, pk=id)
+    if request.method=="POST":
+        nome = request.POST.get('nome')
+        endereco = request.POST.get('endereco')
+        gravadora.nome = nome
+        gravadora.endereco = endereco
+        gravadora.save()
+        return redirect('lista-gravadoras')
+    return render(request, 'form_gravadoras.html', {'gravadora': gravadora})
